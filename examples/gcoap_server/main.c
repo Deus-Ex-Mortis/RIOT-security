8286572d59b97eaf93bf6f4b5f17f0995aa02385
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2021-11-24 17:23:24 +0100

    copyright note updated

--- examples/gcoap_server/main.c
+++ examples/gcoap_server/main.c
@@ -3,9 +3,10 @@
  * @{
  *
  * @file
- * @brief       gcoap example
+ * @brief       Demonstration of a RIOT-AKA authentication over CoAP
  *
  * @author      Alberto La Rosa <albertolarosa98@gmail.com>
+ * @author      Gabriele Restuccia <gabriele.restuccia@cnit.it>
  *
  * @}
  */
